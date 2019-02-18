361292415bf5499270772b2e80f4ce4da656ea8f
luis14051994 <luis14051994>
2019-02-18 17:43:30 -0500

    actualice el vars.tf

---
+++ provider.tf
@@ -0,0 +1,18 @@
+# En este archivo, se define los detalles del proveedor a usar por Terraform
+# El nombre del archivo es sugerido, pues puede ser diferente, siempre que se
+# mantenga la extension .tf
+
+# Bloque de proveedor
+provider "aws" {
+    # El Access Key ID se lee de la variable AWS_ACCESS_KEY
+    # La variable y su valor se definen en otros archivos
+    access_key = "${var.AWS_ACCESS_KEY}"
+
+    # El Secret Key se lee de la variable AWS_SECRET_KEY
+    # La variable y su valor se definen en otros archivos
+    secret_key = "${var.AWS_SECRET_KEY}"
+
+    # La region se lee de la variable AWS_REGION
+    # La variable y su valor se definen en otros archivos
+    region = "${var.AWS_REGION}"
+}
