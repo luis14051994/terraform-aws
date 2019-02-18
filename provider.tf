7b5bf0c0db0fc5cf77b7afda8c19354e5a1c2e37
luis14051994 <luis14051994>
2019-02-18 17:44:30 -0500

    modifique el archivo provider.tf (v1)

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
-# En este archivo, se define los detalles del proveedor a usar por Terraform
+# En este archivo, se define los detalles del proveedor a usar por Terraform prueba1
 # El nombre del archivo es sugerido, pues puede ser diferente, siempre que se
 # mantenga la extension .tf
 
